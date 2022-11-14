012273620338989a2c14d7351260672acd266264
Daniel <daniel.quishpe@epn.edu.ec>
2022-11-14 18:25:45 -0500

    Instancia Daniel Quishpe

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # Carapaz Mendoza Miguel Angel (MiguelCarapaz)
 # Congo Folleco Mateo Sebastian
 # Chasi Galarraga Alexis Fernando (AlexisChasi)
-# Quishpe Guañuna Daniel Andres
+# Quishpe Guañuna Daniel Andres (DAQG)
 # Uchuari Quiñonez Pablo Gustavo (Clarks1223)
 # Guayanay Tixi Wilson Alexander (WilsonG08)
 # Panchi Melo Jose Rafael
--- main.java
+++ main.java
@@ -89,5 +89,18 @@ public class main {
         System.out.println("Codigo Unico: " + est.getCodi());
         System.out.printf("Usuario GitHub : " + est.getUsuario());
         System.out.println("");
+
+        System.out.println("==========================");
+        est.setNombre("Daniel");
+        est.setApellido("Quishpe");
+        est.setEdad(20);
+        est.setCodi("08");
+        est.setUsuario("DAQG");
+        System.out.println("Nombre: " + est.getNombre());
+        System.out.println("Apellido: " + est.getApellido());
+        System.out.println("Edad: " + est.getEdad());
+        System.out.println("Codigo Unico: " + est.getCodi());
+        System.out.printf("Usuario GitHub : " + est.getUsuario());
+        System.out.println("");
     }
 }
