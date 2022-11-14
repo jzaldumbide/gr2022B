a24e10292d85ef4247875dedcbdd03ab3023acf4
AlexisChasi <alexis.chasi@epn.edu.ec>
2022-11-14 12:38:19 -0500

    Instancia AlexisCHasi

--- main.java
+++ main.java
@@ -24,6 +24,19 @@ public class main {
         System.out.printf("Usuario GitHub : "+est.getUsuario());
         System.out.println("");
 
+        System.out.println("==========================");
+        est.setNombre("Alexis");
+        est.setApellido("Chasi");
+        est.setEdad(27);
+        est.setCodi("16");
+        est.setUsuario("AlexisChasi");
+        System.out.println("Nombre: "+est.getNombre());
+        System.out.println("Apellido: "+est.getApellido());
+        System.out.println("Edad: "+est.getEdad());
+        System.out.println("Codigo Unico: "+est.getCodi());
+        System.out.printf("Usuario GitHub : "+est.getUsuario());
+        System.out.println("");
+
 
 
     }
--- out/production/gr22022B/README.md
+++ out/production/gr22022B/README.md
@@ -1,1 +1,9 @@
 # Par
+# Carapaz Mendoza Miguel Angel
+# Congo Folleco Mateo Sebastian
+# Chasi Galarraga Alexis Fernando (AlexisChasi)
+# Quishpe Guañuna Daniel Andres
+# Uchuari Quiñonez Pablo Gustavo
+# Guayanay Tixi Wilson Alexander
+# Panchi Melo Jose Rafael
+# Guanoluisa Peralta Elvis Alexander
