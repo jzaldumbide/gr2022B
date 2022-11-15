cd83d331372bd0860c60b80077db362ec98cb7e6
alexis <alexis.fercho@hotmail.com>
2022-11-14 20:53:54 -0600

    requisito adicional mensaje ejercicio Github

--- main.java
+++ main.java
@@ -35,6 +35,7 @@ public class main {
         System.out.println("Edad: "+est.getEdad());
         System.out.println("Codigo Unico: "+est.getCodi());
         System.out.printf("Usuario GitHub : "+est.getUsuario());
+        System.out.println("ejercicio GitHub");
         System.out.println("");
 
         System.out.println("==========================");
