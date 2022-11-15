bcb876d90bb89114c0109d7ad2b89bc76420b023
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-15 11:47:07 -0500

    metodo UchuariPablo

--- Metodos.java
+++ Metodos.java
@@ -17,4 +17,22 @@ public class Metodos {
         System.out.println("---------------------");
         System.out.println("");
     }
+    public void nom_Uchuari() {
+        Estudiantes est = new Estudiantes();
+        System.out.println("==========================");
+        est.setNombre("Pablo");
+        est.setApellido("Uchuari");
+        est.setEdad(18);
+        est.setCodi("12");
+        est.setUsuario("Clarks1223");
+        System.out.println("Nombre: "+est.getNombre());
+        System.out.println("Apellido: "+est.getApellido());
+        System.out.println("Edad: "+est.getEdad());
+        System.out.println("Codigo Unico: "+est.getCodi());
+        System.out.println("Usuario GitHub : " + est.getUsuario());
+        System.out.println("---------------------");
+        System.out.println("| Ejercicio GitHub  |");
+        System.out.println("---------------------");
+        System.out.println("");
+    }
 }
--- main.java
+++ main.java
@@ -5,5 +5,6 @@ public class main {
 
         Metodos metodos= new Metodos();
         metodos.nom_Chasi();
+        metodos.nom_Uchuari();
     }
 }
